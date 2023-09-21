b834b1b3bc2d0ddaf738f80d6317118d7c7bb7fc
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-09-21 11:25:58 +0100

    feat: add initial version

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,17 @@
 export {}
-console.log("HELLO WORLD FROM BGSCRIPTS")
+
+async function clearCache(): Promise<chrome.webRequest.BlockingResponse> {
+  if (typeof chrome.browsingData === "undefined") {
+    throw new Error("Your browser does not support cache cleaning")
+  }
+
+  await chrome.browsingData.removeCache({})
+
+  return {}
+}
+
+chrome.webRequest.onBeforeRequest.addListener(clearCache, {
+  urls: ["<all_urls>"]
+})
+
+console.log("Cache cleaner is running")
