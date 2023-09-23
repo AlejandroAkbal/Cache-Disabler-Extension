ca89deb2f2e253fc678289dd5f37821184098005
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-09-23 20:57:45 +0100

    refactor: use plasmo storage

--- src/composables/use-hostnames-with-cache-disabled.ts
+++ src/composables/use-hostnames-with-cache-disabled.ts
@@ -1,56 +1,54 @@
-import browser from "webextension-polyfill";
-import { ref, watch } from "vue";
-import { isEqual } from "lodash-es";
+import { isEqual } from 'lodash-es'
+import { ref, toRaw, watch } from 'vue'
+
+import { Storage } from '@plasmohq/storage'
+
+
+
+
 
 export enum StorageKey {
-  hostnamesWithCacheDisabled = "hostnamesWithCacheDisabled"
+  hostnamesWithCacheDisabled = 'hostnamesWithCacheDisabled'
 }
 
-const browserStorage = {
-  get: async (key: StorageKey) => {
-    const storage = await browser.storage.sync.get();
-
-    return storage[key];
-  },
-
-  set: async (key: StorageKey, value: any) => {
-    await browser.storage.sync.set({ [key]: [...value] });
-  }
-}
-
+const storage = new Storage()
 
 export function useHostnamesWithCacheDisabled() {
-
   const hostnamesWithCacheDisabled = ref<string[]>([])
 
-  watch(hostnamesWithCacheDisabled, (newValue, oldValue) => {
+  // Save the value to storage when it changes
+  watch(hostnamesWithCacheDisabled, async (newValue, oldValue) => {
+    newValue = toRaw(newValue)
+    oldValue = toRaw(oldValue)
+
     if (isEqual(newValue, oldValue)) {
-      return;
+      return
     }
 
-    browserStorage.set(StorageKey.hostnamesWithCacheDisabled, newValue)
+    // TODO: Skip until initial value is set
+
+    await storage.set(StorageKey.hostnamesWithCacheDisabled, newValue)
+
+    console.debug('watch: hostnamesWithCacheDisabled changed', { newValue, oldValue })
   })
 
-  browser.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName !== 'sync') {
-      return;
+  // Watch for changes to the storage
+  storage.watch({
+    [StorageKey.hostnamesWithCacheDisabled]: (changes) => {
+      hostnamesWithCacheDisabled.value = changes.newValue
     }
-
-    if (!(StorageKey.hostnamesWithCacheDisabled in changes)) {
-      return;
-    }
-
-    hostnamesWithCacheDisabled.value = changes[StorageKey.hostnamesWithCacheDisabled].newValue
   })
 
-  browserStorage.get(StorageKey.hostnamesWithCacheDisabled)
-    .then((value) => {
-      if (value == null) {
-        return;
-      }
+  // Load the initial value from storage
+  storage.get(StorageKey.hostnamesWithCacheDisabled).then((value) => {
+    if (value == null) {
+      return
+    }
 
-      hostnamesWithCacheDisabled.value = value;
-    })
+    hostnamesWithCacheDisabled.value = value
+
+    console.debug('Loaded hostnamesWithCacheDisabled from storage', value)
+  })
 
   return {
     hostnamesWithCacheDisabled
